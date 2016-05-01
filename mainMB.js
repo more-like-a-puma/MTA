50f08979ec92f35d06f1278616e7646c1eafd329
mr-black-8 <boz.black@gmail.com>
2016-05-01 11:28:13 +1000

    mainMB.js correctly resolves if end station is 'Union Square' on different line

--- mainMB.js
+++ mainMB.js
@@ -1,3 +1,26 @@
+// There are 3 subway lines:
+// The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
+// The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
+// The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
+// All 3 subway lines intersect at Union Square, but there are no other intersection points. (For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.)
+// Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.
+
+
+// We will have three arrays, one for each line. Each array will list stations on that line as the values of the arrays.
+// Begin with travel through one array only...
+// Two variables will be required, startStation & endStation.
+// find indexOf startStation & endStation
+// if startStation < endStation loop forward ++
+// if startStation > endStation loop backward --
+// Loop will add each iteration to a string, used to print stops passed
+// Loop will add to a count variable with each iteration used to print total stops
+
+// Expand for cross - line jurneys....
+// MTA Object containing three array's, one for each line?
+
+// USER INPUT (variables provided) WE CAN WORK WITH INCLUDES:
+// startStation, whatLineTheyStartOn, endStation, whatLineTheyEndOn
+
 var lineN = [
   "Times Square",
   "34th",
@@ -35,6 +58,11 @@ var planTrip = function ( startLine, startStation, endLine, endStation ) {
 
     startIndex = startLine.indexOf(startStation) + 1;
     endIndex = startLine.indexOf(endStation);
+    lineTravel( startLine, startIndex, endIndex );
+  } else if (endStation === "Union Square") {
+
+    startIndex = startLine.indexOf(startStation) + 1;
+    endIndex = startLine.indexOf("Union Square");
     lineTravel( startLine, startIndex, endIndex );
   } else {
 
